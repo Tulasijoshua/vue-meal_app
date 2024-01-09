e308c819d5ab38d8f30027b9ef1c130549a907b0
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-09 14:41:57 +0000

    Feat: by-letter and by-ingredient store

--- src/store/state.js
+++ src/store/state.js
@@ -1,3 +1,5 @@
 export default {
-    searchMeals: [],
+    searchedMeals: [],
+    mealsByLetter: [],
+    mealsByIngredient: [],
 }
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>
-        By letter
+    <div class="flex justify-center gap-2 mt-2:">
+        <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+            {{ letter }}
+        </router-link>
     </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+
+    
 </script>
 
 <style scoped>
