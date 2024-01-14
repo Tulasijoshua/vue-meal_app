7360c9b9384fc2a35dc271401460e17b2b3df435
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-01-14 16:51:37 +0000

    Feat: update

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="p-8 pb-0">
+        <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
+    </div>
+    <div class="px-8 pb-3">
         <input type="text" 
             v-model="keyword"
-            class="w-full rounded border-2 bg-white border-gray-200" 
+            class="w-full rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500" 
             placeholder="Search for Meals" 
             @change="searchMeals"
         />
